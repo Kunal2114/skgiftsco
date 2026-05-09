2032ea0efadca9fcd2174fc25880bd881f967e58
Kunal Kishore <37113220+Kunal2114@users.noreply.github.com>
2026-05-09 23:41:10 +0530

    Create corporate_gifting.sql

---
+++ database/corporate_gifting.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE corporate_gifting;
+
+CREATE TABLE inquiries(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+name VARCHAR(100),
+
+email VARCHAR(100),
+
+phone VARCHAR(20),
+
+message TEXT
+
+);
